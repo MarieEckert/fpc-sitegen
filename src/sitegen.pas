6493a85a629f21e3abc130e06d3e22cb409b5453
marie <marie@ha.si>
2024-05-17 16:03:54 +0200

    begin implementing template parsing

--- src/sitegen.pas
+++ src/sitegen.pas
@@ -1,11 +1,30 @@
 program sitegen;
 
+{
+  fpc-sitegen -- A more featured replacement for sadhtml
+
+  ABOUT
+  fpc-sitgen takes in a simple-ansi-document (see https://github.com/FelixEcker/sad) and a
+  .sgt (sitegen template) to generate a final document.
+
+  Templates define formatting for every kind of SAD switch, see data/template.sgt for a simple
+  example of a template to convert to HTML.
+}
+
 uses uGenerator;
 
 const
   SOURCE = 'data/example.sad';
   TEMPLATE = 'data/template.sgt';
   DESTINATION = 'test/index.html';
+var
+  res: TGenResult;
 begin
-  uGenerator.GenerateSingle(SOURCE, TEMPLATE, DESTINATION); 
+  res := uGenerator.GenerateSingle(SOURCE, TEMPLATE, DESTINATION); 
+
+  if res.is_ok then
+    halt;
+
+  writeln('Generation Error: ', res.err);
+  writeln('==> ', res.err_msg);
 end.
--- src/uTemplate.pas
+++ src/uTemplate.pas
@@ -4,10 +4,30 @@ unit uTemplate;
 
 interface
 
+uses SysUtils;
+
 type
   TTemplateError = (teNONE, teNOT_FOUND, tePARSING_ERROR);
   
+  {
+    Template formatting works by inserting $$CONTENT$$ into your format definition.
+    Everything which comes before $$CONTENT$$ is saved as the prefix_text, everything after is
+    stored as the postfix_text.
+    This means that you can not insert the contents multiple times.
+  }
+  TTemplateFormat = record
+    prefix_text  : String;
+    postfix_text : String;
+  end;
+
   TTemplate = record
+    head_format     : TTemplateFormat;
+    sub_head_format : TTemplateFormat;
+    text_format     : TTemplateFormat;
+    section_format  : TTemplateFormat;
+
+    { Final format for outputting the finished parsed text }
+    output_format   : TTemplateFormat;
   end;
 
   TTemplateResult = record
@@ -17,12 +37,33 @@ type
     err_msg : String;
   end;
 
+  TParseState = (psNONE, psHEAD_FORMAT, psSUB_HEAD_FORMAT, psTEXT_FORMAT, psSECTION_FORMAT,
+                 psOUTPUT_FORMAT);
+
 function Parse(const src: String): TTemplateResult;
 
 implementation
 
 function Parse(const src: String): TTemplateResult;
+var
+  state: TParseState;
+  res: TTemplate;
+  input_file: TextFile;
 begin
+  Parse.is_ok   := True;
+  Parse.value   := res;
+  Parse.err     := teNONE;
+  Parse.err_msg := '';
+
+  if not FileExists(src, True) then
+  begin
+    Parse.is_ok   := False;
+    Parse.err     := teNOT_FOUND;
+    Parse.err_msg := 'can''t open template: No such file or directory.';
+    exit;
+  end;
+
+  state := psNONE;
 end;
 
 end.
